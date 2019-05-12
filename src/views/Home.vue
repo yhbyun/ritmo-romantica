5a8f59fe8cee8292b2c5c581b1ff6a88874682ba
YongHun Byun <yonghunbyun@gmail.com>
2019-05-12 19:22:26 +0900

    Update layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="bg-black text-white flex-1 ml-56 px-10">
+    <div class="bg-black text-white flex-1 ml-56 p-5">
 
-        <div class="flex flex-wrap -mx-2 mb-8">
+        <div class="flex flex-wrap -mx-2">
             <div class="w-full lg:w-1/2 px-2">
-                <div class="border h-screen text-sm text-grey-dark flex items-center justify-center">
+                <div class="border h-full text-sm text-grey-dark flex items-center justify-center">
                     <iframe src="https://ritmoromantica.pe/radioenvivo" class="w-full h-full"></iframe>
                 </div>
             </div>
             <div class="w-full lg:w-1/2 px-2">
                 <div class="w-full mb-4">
-                    <div class="border h-full text-sm text-grey-dark flex items-center justify-center" style="height: 22rem;">
+                    <div class="border h-88 text-sm text-grey-dark flex items-center justify-center">
                         <iframe src="https://translate.google.com/" class="w-full h-full"></iframe>
                     </div>
                 </div>
                 <div class="w-full">
-                    <div class="border h-full text-sm text-grey-dark flex items-center justify-center" style="height: 22rem;">
+                    <div class="border h-80 text-sm text-grey-dark flex items-center justify-center">
                         <iframe src="https://dict.naver.com/eskodict/#/main" class="w-full h-full"></iframe>
                     </div>
                 </div>
