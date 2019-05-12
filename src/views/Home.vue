f9c025e09358245f6588f349acf88908fadaebe5
YongHun Byun <yonghunbyun@gmail.com>
2019-05-12 23:40:35 +0900

    Use vue-grid-layout in order to drag & resize

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,55 @@
 <template>
-    <div class="bg-black text-white flex-1 p-5 h-full">
-
-        <div class="flex flex-wrap -mx-2 h-full">
-            <div class="w-full lg:w-1/2 px-2">
-                <div class="border h-full text-sm text-grey-dark flex items-center justify-center">
-                    <iframe src="https://ritmoromantica.pe/radioenvivo" class="w-full h-full"></iframe>
-                </div>
+    <grid-layout
+            :layout.sync="layout"
+            :col-num="12"
+            :row-height="30"
+            :is-draggable="true"
+            :is-resizable="true"
+            :is-mirrored="false"
+            :vertical-compact="true"
+            :margin="[10, 10]"
+            :use-css-transforms="true"
+    >
+        <grid-item v-for="item in layout" v-bind:key="item.i"
+                    :x="item.x"
+                    :y="item.y"
+                    :w="item.w"
+                    :h="item.h"
+                    :i="item.i">
+            <div class="w-full h-full" v-if="item.i === '0'">
+                <iframe src="https://ritmoromantica.pe/radioenvivo" class="w-full h-full"></iframe>
             </div>
-            <div class="w-full lg:w-1/2 px-2">
-                <div class="w-full pb-4" style="height:50%">
-                    <div class="border h-full text-sm text-grey-dark flex items-center justify-center">
-                        <iframe src="https://translate.google.com/" class="w-full h-full"></iframe>
-                    </div>
-                </div>
-                <div class="w-full" style="height:50%">
-                    <div class="border h-full text-sm text-grey-dark flex items-center justify-center">
-                        <iframe src="https://dict.naver.com/eskodict/#/main" class="w-full h-full"></iframe>
-                    </div>
-                </div>
+            <div class="w-full h-full" v-else-if="item.i === '1'">
+                <iframe src="https://translate.google.com/" class="w-full h-full"></iframe>
             </div>
-        </div>
+            <div class="w-full h-full" v-else-if="item.i === '2'">
+                <iframe src="https://dict.naver.com/eskodict/" class="w-full h-full"></iframe>
+            </div>
+        </grid-item>
+    </grid-layout>
 
-    </div>
 </template>
 
 <script>
+import VueGridLayout from 'vue-grid-layout';
+
+const layout = [
+    {"x":0,"y":0,"w":6,"h":18,"i":"0"},
+    {"x":6,"y":0,"w":6,"h":9,"i":"1"},
+    {"x":6,"y":9,"w":6,"h":9,"i":"2"},
+];
+
 // @ is an alias to /src
 export default {
     name: 'home',
+    components: {
+        GridLayout: VueGridLayout.GridLayout,
+        GridItem: VueGridLayout.GridItem
+    },
+    data: function () {
+        return {
+	        layout: layout,
+        }
+    },
 }
 </script>
