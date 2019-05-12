70fc29bfa3cbf6ac53581c8404926e4bfab41409
YongHun Byun <yonghunbyun@gmail.com>
2019-05-12 17:01:00 +0900

    Prevent blank screen in Electron builds

--- src/main.js
+++ src/main.js
@@ -8,5 +8,9 @@ Vue.config.productionTip = false
 new Vue({
     router,
     store,
-    render: h => h(App)
+    render: h => h(App),
+    mounted() {
+        // Prevent blank screen in Electron builds
+        this.$router.push('/')
+    }
 }).$mount('#app')
