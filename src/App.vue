e9542be2fc971b40c23ad62959f57e5069f778d6
YongHun Byun <yonghunbyun@gmail.com>
2019-05-12 20:22:50 +0900

    Add togglable sidebar
    https://medium.com/@rmmmsy/creating-an-animated-sidebar-component-with-vue-and-vuex-9f7c6aa19fce
    참고

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
     <div id="app" style="-webkit-app-region: drag" class="font-sans flex min-h-screen">
-        <sidebar></sidebar>
-        <router-view/>
+        <div :class="$style.container">
+            <router-view/>
+        </div>
+        <sidebar/>
+        <sidebar-toggle/>
     </div>
 </template>
 
 <script>
-import Sidebar from './components/Sidebar.vue'
+import Sidebar from '@/components/sidebar.vue'
+import SidebarToggle from '@/components/sidebarToggle.vue'
 
 export default {
     components: {
-        Sidebar,
+        Sidebar, SidebarToggle
     }
 }
 </script>
 
-<style src="./assets/main.css">
+<style src="./assets/main.css" />
+
+<style>
+    :root{
+        --accent-color: #FFCB08;
+        --primary-color: #820263;
+        --dark-color: #2E294E;
+    }
+
+    *{
+        box-sizing: border-box;
+    }
+</style>
+
+<style module>
+    .container{
+        position: fixed;
+        left: 0;
+        top: 0;
+        height: 100vh;
+        width: 100vw;
+        background-color: var(--primary-color);
+    }
+</style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,9 @@
 <template>
+    <!--
     <div class="w-56 bg-grey-darkest text-white border-r border-blackblack fixed min-h-screen pt-8">
+    -->
+    <div :class="$style.sidebar">
+
         <div class="search-container text-center p-4 relative">
             <input type="text" placeholder="Search" class="bg-grey-darker text-grey-light text-xs w-full p-2 pl-8 border border-grey-dark rounded">
             <div class="absolute pin-t py-5 px-2 text-grey-light">
@@ -21,7 +25,45 @@
                 </svg>
                 <div class="text-white">Create</div>
             </div> <!-- end sidebar-icon -->
-
         </div>
+
     </div>
 </template>
+
+<script>
+    import {TweenMax, Power4} from 'gsap'
+    export default {
+        name: 'sidebar',
+        mounted () {
+            TweenMax.set(this.$el, {
+                x: this.$el.offsetWidth
+            })
+        },
+        computed: {
+            open () {
+                return this.$store.state.ui.sidebarOpen
+            }
+        },
+        watch: {
+            open: function (open) {
+                const dX = open ? 0 : this.$el.offsetWidth
+                TweenMax.to(this.$el, 0.6, {
+                    x: dX,
+                    ease: Power4.easeOut
+                })
+            }
+        }
+    }
+</script>
+
+<style module>
+    .sidebar{
+        position: fixed;
+        right: 0;
+        top: 0;
+        width: 300px;
+        height: 100vh;
+        max-width: 90vw;
+        background-color: var(--accent-color);
+    }
+</style>
